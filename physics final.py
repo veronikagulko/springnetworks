fcdc02c030f4d99f87b2ff113a05e9c41294042c
mvuong60 <mvuong60@stuy.edu>
2026-05-20 13:57:55 -0400

    click screen to create nodes

--- physics final.py
+++ physics final.py
@@ -1,1 +1,26 @@
 from vpython import *
+
+#Web VPython 3.2
+
+scene = canvas(
+    title="Click to Draw Circles",
+    width=800,
+    height=600,
+    background=color.black
+)
+
+scene.range = 5
+scene.center = vec(0, 0, 0)
+
+click_surface = box(
+    pos=vec(0, 0, 0),
+    size=vec(20, 20, 0.01),
+    color=color.black
+)
+
+def draw_circle(evt): #circle draw command
+    click_pos = evt.pos
+    sphere(pos=click_pos + vec(0,0, 0.05), radius = 0.5, color = color.white)
+    
+scene.bind("mousedown", draw_circle)
+
