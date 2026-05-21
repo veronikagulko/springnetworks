c48e5e619ec561d17c46e66beae5e0650d3f1117
veronikagulko <102479004+veronikagulko@users.noreply.github.com>
2026-05-21 13:37:19 -0400

    add in button for clearing screen, added in custom lights to understand view (red is front, cyan is back)

--- physics final.py
+++ physics final.py
@@ -18,9 +18,31 @@ click_surface = box(
     color=color.black
 )
 
-def draw_circle(evt): #circle draw command
+# delete light source
+scene.lights = []
+# custom light source
+my_light = local_light(pos=vector(5, 5, 5), color=color.red)
+my_light = local_light(pos=vector(5, 5, -5), color=color.cyan)
+
+
+# button for clear screen
+def clear_action(b):
+    for obj in scene.objects:
+        obj.visible = False
+        obj.delete()
+        
+button(bind=clear_action, text="Clear Screen")
+
+
+
+circle_positions = []
+
+def draw_circle(evt):
     click_pos = evt.pos
     sphere(pos=click_pos + vec(0,0, 0.05), radius = 0.5, color = color.white)
+    circle_positions.append(click_pos)
+
+
     
 scene.bind("mousedown", draw_circle)
 
