96430997b10fa028febb533a639fad6288db13db
veronikagulko <102479004+veronikagulko@users.noreply.github.com>
2026-05-22 14:25:47 -0400

    updated variables, getting ready to write acceleration due to gravity. removed user zoom

--- physics final.py
+++ physics final.py
@@ -1,22 +1,17 @@
 from vpython import *
 
-# Web VPython 3.2
-
 scene = canvas(
     title="Click to Draw Circles",
     width=800,
     height=600,
     background=color.black
 )
+scene.userzoom = False
 
 scene.range = 5
 scene.center = vec(0, 0, 0)
 
-click_surface = box(
-    pos=vec(0, 0, 0),
-    size=vec(20, 20, 0.01),
-    color=color.black
-)
+#click_surface = box(pos=vec(0, 0, 0), size=vec(20, 20, 0.01), color=color.black)
 
 # delete default light sources
 scene.lights = []
@@ -30,8 +25,13 @@ radius = 0.5
 neighbors = 2
 
 circles = []
-springs = []
+velocities = []
 circle_positions = []
+
+springs = []
+spring_pairs = []
+spring_rest_lengths = []
+
 
 def clear_action(b):
     for circle in circles:
@@ -43,20 +43,22 @@ def clear_action(b):
         spring.delete()
 
     circles.clear()
-    circle_positions.clear()
+    velocities.clear()
     springs.clear()
+    spring_pairs.clear()
+    spring_rest_lengths.clear()
 
 button(bind=clear_action, text="Clear Screen")
 
 def not_overlapping_nodes(new_pos):
-    for old_pos in circle_positions:
-        if mag(old_pos - new_pos) < radius * 2:
+    for circle in circles:
+        if mag(circle.pos - new_pos) < radius * 2:
             return False
     return True
 
 def spring_generator():
     # exit function if there are less than 2 circles on the screen
-    if len(circle_positions) < 2:
+    if len(circles) < 2:
         return
 
     # clear old springs
@@ -64,27 +66,29 @@ def spring_generator():
         spring.visible = False
         spring.delete()
     springs.clear()
-
+    spring_pairs.clear()
+    spring_rest_lengths.clear()
+    
     # use set so we do not duplicate springs
-    spring_pairs = set()
-
-    for i in range(len(circle_positions)):
+    springs_pairs = set()
+    
+    for i in range(len(circles)):
         distances_btwn_circles = []
 
-        for j in range(len(circle_positions)):
+        for j in range(len(circles)):
             if i != j:
-                distance = mag(circle_positions[i] - circle_positions[j])
+                distance = mag(circles[i].pos - circles[j].pos)
                 distances_btwn_circles.append((distance, j))
 
         distances_btwn_circles.sort()
 
         for distance, j in distances_btwn_circles[:neighbors]:
-            spring_pairs.add(tuple(sorted((i, j))))
+            spring_pairs.append(tuple(sorted((i, j))))
 
     for i, j in spring_pairs:
         new_spring = helix(
-            pos=circle_positions[i],
-            axis=circle_positions[j] - circle_positions[i],
+            pos=circles[i].pos,
+            axis=circles[j].pos - circles[i].pos,
             radius=0.08,
             thickness=0.03,
             color=color.yellow
@@ -102,7 +106,6 @@ def draw_circle(evt):
             color=color.white
         )
 
-        circle_positions.append(click_pos)
         circles.append(new_sphere)
 
         spring_generator()
@@ -111,3 +114,12 @@ def draw_circle(evt):
         print("Circle overlaps! Not drawing.")
 
 scene.bind("mousedown", draw_circle)
+
+velocity = vec(0,0,0)
+acceleration = -9.8
+position = (1/2)* acceleration * (clock())**2
+
+for circle in circles:
+    while circle.pos > 0:
+        rate(30)
+        circle.pos = circle.pos + vec(0.5, 0.5, 0) * 0.1
